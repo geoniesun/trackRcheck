4e58d442d88b26d135f256d243e4fdc4cdbfff15
geoniesun <leoniesonntag26@gmail.com>
2024-03-25 14:06:46 +0100

    sortening

--- R/minpoint.R
+++ R/minpoint.R
@@ -1,10 +1,10 @@
 
 
 minpoint <- function(raster, tracks, distance_points_along_line = 1) {
-  
-  
-  
-  
+
+
+
+
   #points along geometry = PAG
   result <- qgis_run_algorithm(
     algorithm = "native:pointsalonglines",
@@ -13,10 +13,10 @@ minpoint <- function(raster, tracks, distance_points_along_line = 1) {
   )
   qgis_extract_output(result)
   pag <- sf::st_as_sf(result)
-  
-  
-  
-  
+
+
+
+
   #geometry by expression = GBE ## this creates vertical lines on each point of pag and its original path going through it
   #https://gis.stackexchange.com/questions/380361/creating-perpendicular-lines-on-line-using-qgis
   profiles <- qgis_run_algorithm(
@@ -31,11 +31,11 @@ minpoint <- function(raster, tracks, distance_points_along_line = 1) {
   gbe <- sf::st_as_sf(profiles)
   gbe[["line_id"]] <- 1:nrow(gbe)#defining line_ad as field
   gbe$line_id <- 1:nrow(gbe)#adding line_ad as field to gbe
-  
-  
+
+
   # SAGA Profile from lines = sagaPFL # NEW title: creating points on vertical lines
-  
-  
+
+
   sagaPFL <- qgis_run_algorithm(
     algorithm = "native:pointsalonglines",
     INPUT = gbe,
@@ -43,5 +43,6 @@ minpoint <- function(raster, tracks, distance_points_along_line = 1) {
   )
   qgis_extract_output(sagaPFL)
   pfl <- sf::st_as_sf(sagaPFL)
-  
+
 }
+

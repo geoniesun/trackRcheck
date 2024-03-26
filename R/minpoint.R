ca883fdeb54624c6d1ab84b1ab6606c166ef9ee0
geoniesun <leoniesonntag26@gmail.com>
2024-03-26 10:44:18 +0100

    centerpoints work now and colnames are fixed

--- R/minpoint.R
+++ R/minpoint.R
@@ -77,25 +77,52 @@ minpoint <- function(raster, tracks, distance_points_along_line = 1, profileleng
 
   centerpoints <- sf::st_centroid(joinedL)
 
-
+  # adding the dsm values to the points
   dsmpoints <- terra::extract(raster,centerpoints)
   centerpoints$z <- dsmpoints[, -1]
 
+
+
+  #removing the unnecessary line_id.y.x
+  centerpoints <- centerpoints %>%
+    dplyr::select(!ends_with("y"))
+
+   centerpoints <-  centerpoints %>%
+                        dplyr::rename(
+                          class_id = class_id.x,
+                          line_id = line_id.x,
+                          fade_scr = fade_scr.x,
+                          distance = distance.x,
+                          angle = angle.x
+    )
+
+
   #categorial statistics
 
-  catstats <- qgis_run_algorithm(
-    algorithm = "qgis:statisticsbycategories",
-    INPUT = centerpoints,
-    VALUES_FIELD_NAME = "z",
-    CATEGORIES_FIELD_NAME = "line_id"
-
-  )
-  s <- qgis_extract_output(catstats)
-  stats <- sf::st_as_sf(s)
+ # catstats <- qgis_run_algorithm(
+   # algorithm = "qgis:statisticsbycategories",
+   # INPUT = centerpoints,
+   # VALUES_FIELD_NAME = "z",
+   # CATEGORIES_FIELD_NAME = "line_id"
 
 
 
-  return(stats)
+
+ # )
+
+
+
+  return(centerpoints)
 
 }
 
+centerpoints <- centerpoints %>%
+ # dplyr::select(-line_id.y) %>%
+  dplyr::rename(line_id = line_id.x)
+
+s <- qgis_extract_output(catstats)
+stats <- sf::st_as_sf(s)
+
+
+
+minpoint(raster,tracks)
